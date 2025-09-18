59511503a27a0ad15745390532d68e3f831f44fa
jhuliangr <jhpiano9731@gmail.com>
2025-09-19 01:57:01 +0200

    Making every endpoint accesible

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ const router = require("express").Router();
 router.get("/", lockers.checkState);
 router.get("/:id", lockers.requestOpenLocker);
 router.post("/:id", lockers.setPasswordAndLock);
+router.post("/:id/unlock", lockers.unlock);
+router.post("/:id/canBeOpened", lockers.checkIfCanBeOpened);
+
 module.exports = router;

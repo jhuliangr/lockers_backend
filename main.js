4ea1be764f444bbc0c228f5cd81c8dcb62fe3474
jhuliangr <jhpiano9731@gmail.com>
2025-09-19 02:14:39 +0200

    Handling cors

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/lockers", require("./routes"));
 app.use(express.static(path.join(__dirname, "./public")));
